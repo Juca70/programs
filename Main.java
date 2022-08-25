6f94028e49ce6f301d3f20dc24f85b119df9e6ba
MAQC03 <MAQC03@xsistemas.local>
2022-08-25 16:09:27 -0700

    new java file to display a window on screen

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+
+public class Main {
+
+	static public void main(string[] args) {
+		JFrame window = new JFrame("window");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(800, 600);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+
+}
